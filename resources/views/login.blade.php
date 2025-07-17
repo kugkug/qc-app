3eb812b252a437da9e88f13321fd94d4b02efa0f
accounts15 <accounts15@kodego.ph>
2025-07-17 10:52:23 +0800

    Fix Announcement

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,6 +3,26 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-8">
+            
+                    <div class="bootstrap-carousel">
+                        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="{{ asset('assets/images/announcement/1.png') }}" alt="First slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="{{ asset('assets/images/announcement/2.png') }}" alt="Second slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="{{ asset('assets/images/announcement/3.png') }}" alt="Third slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="{{ asset('assets/images/announcement/4.png') }}" alt="Fourth slide">
+                                </div>
+                            </div><a class="carousel-control-prev" href="#carouselExampleControls" data-slide="prev"><span class="carousel-control-prev-icon"></span> <span class="sr-only">Previous</span> </a><a class="carousel-control-next" href="#carouselExampleControls" data-slide="next"><span class="carousel-control-next-icon"></span> <span class="sr-only">Next</span></a>
+                        </div>
+                    </div>
+                
         </div>
 
         <div class="col-lg-4">
