22325d4a90552f37d9d58b73c424d6cfd26efdfb
accounts15 <accounts15@kodego.ph>
2025-07-18 01:06:32 +0800

    Fix Contact Us

--- resources/views/partials/applicant/contact_us.blade.php
+++ resources/views/partials/applicant/contact_us.blade.php
@@ -1,24 +1,33 @@
 @include('partials.applicant.header')
 <div class="container">
     <h1>Connect with Us</h1>
-    <p> We would love to respond o your queries and help you succeed. <br>
-        Feel feel free to get in touch with us</p>
+    <p> We would love to respond o your queries and help you succeed. <br> Feel feel free to get in touch with us</p>
+    <div class="row mt-5">
+
     
-    <div class="mb-4">
-        <h3>Our Location</h3>
-        <div style="width: 100%; height: 400px; max-width: 100%; border-radius: 8px; overflow: hidden; box-shadow: 0 2px 8px rgba(0,0,0,0.1);">
-            <iframe 
-                src="https://www.openstreetmap.org/export/embed.html?bbox=121.04796%2C14.644369%2C121.05196%2C14.648369&amp;layer=mapnik&amp;marker=14.646369,121.04996"
-                width="100%"
-                height="100%"
-                style="border:0;"
-                allowfullscreen=""
-                loading="lazy"
-                title="High Rise Building, Diliman, Quezon City"
-            ></iframe>
+        <div class="col-md-6 bg-primary-system text-center d-flex align-items-center justify-content-center text-white">
+            <div class="">
+              <h2>Quezon City <strong>Health Department</strong></h2>
+              <p>SEND US AN EMAIL AT: <strong>DBO@Quezoncity.gov.ph</strong></p>
+              <p>PLEASE CALL US AT: <strong>(+63)(02)8988-4242 loc 8907</strong></p>
+              <p>OUR MAIN OFFICE: <strong>Ground to 3rd floors, Civic Center D (BRO Bldg.), <br> Mayaman Street, Quezon City Hall Compound</strong></p>
+
+            </div>
+          </div>
+        <div class="col-md-6 p-0">
+            <div style="width: 100%; height: 400px; width: 100%; overflow: hidden; box-shadow: 0 2px 8px rgba(0,0,0,0.1);">
+                <iframe 
+                    src="https://www.openstreetmap.org/export/embed.html?bbox=121.04796%2C14.644369%2C121.05196%2C14.648369&amp;layer=mapnik&amp;marker=14.646369,121.04996"
+                    width="100%"
+                    height="100%"
+                    style="border:0;"
+                    allowfullscreen=""
+                    loading="lazy"
+                    title="High Rise Building, Diliman, Quezon City"
+                ></iframe>
+            </div>
         </div>
     </div>
-
 </div>
 
 @include('partials.applicant.footer')
